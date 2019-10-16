6b9775ab1cd02d54fb0c448a30dfd9e056b0c1aa
1263429730 <52957786+1263429730@users.noreply.github.com>
2019-10-16 19:39:20 +0800

    Update and rename demo-1/src/main/java/com/lkd/controller/HelloWorld.java to 演示-1/SRC/主/爪哇/COM/LKD/控制器/HelloWorld.java
    aaa

--- 演示-1/SRC/主/爪哇/COM/LKD/控制器/HelloWorld.java
+++ 演示-1/SRC/主/爪哇/COM/LKD/控制器/HelloWorld.java
@@ -3,6 +3,7 @@ package com.lkd.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //测试git提交
+//更新git测试
 @RestController
 public class HelloWorld {
 	
