e97d3cf9ab0416afbb0053a09ff6d1133f1416a6
1263429730 <1263429730@qq.com>
2019-10-16 19:18:40 +0800

    发布项目

---
+++ demo-1/src/main/java/com/lkd/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lkd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@RequestMapping("/hello")
+	public String showHelloWorld(){		
+		return "Hello World";
+	}
+}
