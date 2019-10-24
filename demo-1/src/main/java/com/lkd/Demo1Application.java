580a7b8a2749ff4de777144516956241c92b278b
1263429730 <1263429730@qq.com>
2019-10-24 10:18:34 +0800

    测试更新

--- demo-1/src/main/java/com/lkd/Demo1Application.java
+++ demo-1/src/main/java/com/lkd/Demo1Application.java
@@ -2,7 +2,7 @@ package com.lkd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//修改了哦阿斯蒂芬
 @SpringBootApplication
 public class Demo1Application {
 
